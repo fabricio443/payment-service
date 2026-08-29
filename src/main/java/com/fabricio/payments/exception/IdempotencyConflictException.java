25b900c7f52180df2990dac18e39513ab74823b7
Fabricio443 <fabriciohcosta@outlook.com.br>
2026-08-29 20:48:31 -0300

    feat: implement global API exception handling

---
+++ src/main/java/com/fabricio/payments/exception/IdempotencyConflictException.java
@@ -0,0 +1,12 @@
+package com.fabricio.payments.exception;
+
+public class IdempotencyConflictException extends RuntimeException {
+
+    public IdempotencyConflictException(String message) {
+        super(message);
+    }
+
+    public IdempotencyConflictException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
